f0e66a18d31be5b8cdcfb17f9bd804f666811e3c
aocl333 <dulce04@naver.com>
2022-05-26 17:56:52 +0900

    beesbeesbees

---
+++ src/ForagerBee.js
@@ -0,0 +1,15 @@
+const Bee = require('./Bee');
+
+class ForagerBee extends Bee{
+  constructor(){
+    super()
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+  forage(el){
+    return this.treasureChest.push(el)
+  }
+}
+module.exports = ForagerBee;
